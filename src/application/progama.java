26af9030c36730d0e07497fa43e44ba65e61ecba
jopdaltro <jopdaltro@gmail.com>
2022-01-17 04:42:03 -0300

    first class: Posicao

--- src/application/progama.java
+++ src/application/progama.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class progama {
 
 	public static void main(String[] args) {
 		System.out.println("heloo word");
+		Posicao posicao = new Posicao(5,3);
+		System.out.println(posicao);
 
 	}
 
