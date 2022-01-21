3b7185f0bf48120b66a904f71d8fd0db24dec800
jopdaltro <jopdaltro@gmail.com>
2022-01-21 03:25:44 -0300

    limpando a tela e tratando exceções

--- src/application/progama.java
+++ src/application/progama.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaDeXadrez;
 import xadrez.PeçasXadrez;
+import xadrez.XadrezException;
 import xadrez.XadrezPosicao;
 
 public class progama {
@@ -12,18 +14,29 @@ public class progama {
 		Scanner scan = new Scanner(System.in);
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 		
-		while (true){		
-		UI.PrintTabuleiro(partidaDeXadrez.getPeças());
-		System.out.println("");
-		System.out.print("Origem: ");
-		XadrezPosicao origem =UI.lerPosicao(scan);
-		
-		System.out.println();
-		System.out.println("Destino ");
-		XadrezPosicao destino =UI.lerPosicao(scan);
-		
-		PeçasXadrez peçaCapturada =partidaDeXadrez.moverPeça(origem, destino);
-		
+		while (true){	
+		try {
+			UI.clearScreen();
+			UI.PrintTabuleiro(partidaDeXadrez.getPeças());
+			System.out.println("");
+			System.out.print("Origem: ");
+			XadrezPosicao origem =UI.lerPosicao(scan);
+			
+			System.out.println();
+			System.out.println("Destino ");
+			XadrezPosicao destino =UI.lerPosicao(scan);
+			
+			PeçasXadrez peçaCapturada =partidaDeXadrez.moverPeça(origem, destino);
+		}
+		catch(XadrezException e) {
+			System.out.println(e.getMessage());
+			scan.nextLine();
+			
+		}
+		catch(InputMismatchException e) {
+			System.out.println(e.getMessage());
+			scan.nextLine();
+		}
 		
 		}
 	}
